7bc3c4b229b9981adeb4c6017c54d08e9a94da2a
Shetye <shetyer@147dda820123.ant.amazon.com>
2022-01-23 20:14:32 +0530

    Add PreProd stage3

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -30,8 +30,8 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
     // This is where we add the application stages
     // ...
 
-    pipeline.addStage(new CdkpipelinesDemoStage(this, 'PreProd', {
-  env: { account: '997785413584', region: 'us-east-1' }
-   }));
+//    pipeline.addStage(new CdkpipelinesDemoStage(this, 'PreProd', {
+//  env: { account: '997785413584', region: 'us-east-1' }
+//   }));
   }
 }
